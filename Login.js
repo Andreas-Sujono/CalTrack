2c6917322e8f9f27f9e82ad8a23349f53cc40b8d
Andreas_Sujono <andreassujono@gmail.com>
2020-10-06 14:52:51 +0800

    initialize file

---
+++ Login.js
@@ -0,0 +1,227 @@
+import React from 'react';
+import {
+  View,
+  Text
+} from 'react-native';
+
+export default () => {
+  return (
+    <View style = {
+      {
+        "alignItems": "flex-start",
+        "paddingStart": 29,
+        "paddingTop": 49.1,
+        "flex": 1
+      }
+    } >
+    <View style = {
+      {
+        "alignItems": "flex-start",
+        "marginStart": 63
+      }
+    } >
+
+    {
+      /* <Path /> {Path is not supported. It can be exported as Svg} */ }
+    <View style = {
+      {
+        "width": 8.83,
+        "height": 11.41,
+        "backgroundColor": "#000000"
+      }
+    }
+    />
+
+    {
+      /* <Path /> {Path is not supported. It can be exported as Svg} */ }
+    <View style = {
+      {
+        "width": 11.96,
+        "height": 8.29,
+        "backgroundColor": "#000000"
+      }
+    }
+    />
+
+    {
+      /* <Path /> {Path is not supported. It can be exported as Svg} */ }
+    <View style = {
+      {
+        "width": 11.18,
+        "height": 9.33,
+        "backgroundColor": "#000000"
+      }
+    }
+    />
+    <View style = {
+      {
+        "alignItems": "flex-start",
+        "marginStart": 3.99,
+        "marginTop": 12.03,
+        "opacity": 0.1,
+        "width": 6.77,
+        "height": 6.77,
+        "borderRadius": 3.3853232860565186,
+        "transform": [{
+          "scaleX": 2.81
+        }],
+        "backgroundColor": "rgba(0, 0, 0, 255)"
+      }
+    }
+    />
+
+    {
+      /* <Path /> {Path is not supported. It can be exported as Svg} */ }
+    <View style = {
+      {
+        "width": 185.64,
+        "height": 139.04,
+        "backgroundColor": "#000000"
+      }
+    }
+    />
+    </View>
+    <View style = {
+      {
+        "alignItems": "flex-start",
+        "marginTop": 24.86
+      }
+    } >
+    <View style = {
+      {
+        "alignItems": "flex-start",
+        "paddingStart": 16,
+        "paddingTop": 22,
+        "width": 318,
+        "height": 336,
+        "borderRadius": 8,
+        "backgroundColor": "rgba(255, 255, 255, 255)"
+      }
+    } >
+    <View style = {
+      {
+        flexDirection: 'row',
+        alignItems: 'flex-start'
+      }
+    } > <Text style = {
+      {
+        "fontFamily": "Poppins",
+        "fontWeight": "bold",
+        "fontSize": 28,
+        "color": "rgba(43, 43, 43, 255)",
+        "marginStart": 75
+      }
+    } > Welcome </Text>
+    <Text style = {
+      {
+        "fontFamily": "Poppins",
+        "fontSize": 14,
+        "color": "rgba(43, 43, 43, 255)",
+        "marginStart": -133,
+        "marginTop": 35
+      }
+    } > Sign in to
+    continue </Text>
+      </View>
+      <View style = {
+        {
+          "alignItems": "flex-start",
+          "marginTop": 32
+        }
+      } >
+      <View style = {
+        {
+          "alignItems": "flex-start",
+          "paddingStart": 15,
+          "paddingTop": 8,
+          "width": 278,
+          "height": 35,
+          "borderRadius": 8,
+          "borderWidth": 1,
+          "borderColor": "rgba(235, 235, 242, 255)",
+          "backgroundColor": "rgba(252, 252, 255, 255)"
+        }
+      } >
+      <Text style = {
+        {
+          "fontFamily": "Poppins",
+          "fontSize": 14,
+          "color": "rgba(174, 175, 193, 255)"
+        }
+      } > Username </Text>
+      </View>
+      </View>
+      <View style = {
+        {
+          "alignItems": "flex-start",
+          "marginTop": 23
+        }
+      } >
+      <View style = {
+        {
+          "alignItems": "flex-start",
+          "paddingStart": 15,
+          "paddingTop": 8,
+          "width": 278,
+          "height": 35,
+          "borderRadius": 8,
+          "borderWidth": 1,
+          "borderColor": "rgba(235, 235, 242, 255)",
+          "backgroundColor": "rgba(252, 252, 255, 255)"
+        }
+      } >
+      <Text style = {
+        {
+          "fontFamily": "Poppins",
+          "fontSize": 14,
+          "color": "rgba(174, 175, 193, 255)"
+        }
+      } > Password </Text>
+      </View>
+      </View>
+      <View style = {
+        {
+          "alignItems": "flex-start",
+          "marginStart": 3,
+          "marginTop": 23
+        }
+      } >
+      <View style = {
+        {
+          "alignItems": "flex-start",
+          "paddingStart": 113,
+          "paddingTop": 6,
+          "width": 275,
+          "height": 35,
+          "borderRadius": 8,
+          "backgroundColor": "rgba(64, 163, 255, 255)"
+        }
+      } >
+      <Text style = {
+        {
+          "fontFamily": "Poppins",
+          "fontSize": 16,
+          "color": "rgba(255, 255, 255, 255)"
+        }
+      } > Sign In </Text>
+      </View>
+      </View>
+      <Text style = {
+        {
+          "marginStart": 42,
+          "marginTop": 14
+        }
+      } >
+      <Text style = {
+        {
+          "fontFamily": "Poppins",
+          "fontSize": 12,
+          "color": "rgba(33, 33, 33, 255)"
+        }
+      } > Don’ t have account ? </Text><Text style={{"fontFamily":"Poppins","fontSize":12,"color":"rgba(64, 163, 255, 255)"}}>Sign up here</Text > </Text>
+      </View>
+      </View>
+      </View>
+
+  );
+};
