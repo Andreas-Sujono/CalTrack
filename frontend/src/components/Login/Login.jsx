6eaa858ab54175594fa456efaaabe5f4ee70b9a7
Andreas_Sujono <andreassujono@gmail.com>
2020-10-06 19:07:37 +0800

    refactor style login

--- frontend/src/components/Login/Login.jsx
+++ frontend/src/components/Login/Login.jsx
@@ -4,224 +4,53 @@ import {
   Text
 } from 'react-native';
 
+import styles from './Login.style';
+
 export default () => {
   return (
-    <View style = {
-      {
-        "alignItems": "flex-start",
-        "paddingStart": 29,
-        "paddingTop": 49.1,
-        "flex": 1
-      }
-    } >
-    <View style = {
-      {
-        "alignItems": "flex-start",
-        "marginStart": 63
-      }
-    } >
+    <View style = {styles.container}>
 
-    {
-      /* <Path /> {Path is not supported. It can be exported as Svg} */ }
-    <View style = {
-      {
-        "width": 8.83,
-        "height": 11.41,
-        "backgroundColor": "#000000"
-      }
-    }
-    />
+      <View style = {styles.container2}>
+        <View style = {styles.container3}/>
+        <View style = {styles.container4}/>
+        <View style = {styles.container5}/>
+        <View style = {styles.container6}/>
+        <View style = {styles.container7}/>
+      </View>
 
-    {
-      /* <Path /> {Path is not supported. It can be exported as Svg} */ }
-    <View style = {
-      {
-        "width": 11.96,
-        "height": 8.29,
-        "backgroundColor": "#000000"
-      }
-    }
-    />
+      <View style = {styles.container8}>
+        <View style = {styles.container9}>
 
-    {
-      /* <Path /> {Path is not supported. It can be exported as Svg} */ }
-    <View style = {
-      {
-        "width": 11.18,
-        "height": 9.33,
-        "backgroundColor": "#000000"
-      }
-    }
-    />
-    <View style = {
-      {
-        "alignItems": "flex-start",
-        "marginStart": 3.99,
-        "marginTop": 12.03,
-        "opacity": 0.1,
-        "width": 6.77,
-        "height": 6.77,
-        "borderRadius": 3.3853232860565186,
-        "transform": [{
-          "scaleX": 2.81
-        }],
-        "backgroundColor": "rgba(0, 0, 0, 255)"
-      }
-    }
-    />
+          <View style = {styles.container10} > 
+            <Text style = {styles.container11} > Welcome </Text>
+            <Text style = {styles.container12} > Sign in to continue </Text>
+          </View>
 
-    {
-      /* <Path /> {Path is not supported. It can be exported as Svg} */ }
-    <View style = {
-      {
-        "width": 185.64,
-        "height": 139.04,
-        "backgroundColor": "#000000"
-      }
-    }
-    />
+
+          <View style = {styles.container13}>
+            <View style = {styles.container14}>
+              <Text style = {styles.container15} > Username </Text>
+            </View>
+          </View>
+
+          <View style = {styles.container16} >
+            <View style = {styles.container17} >
+              <Text style = {styles.container18} > Password </Text>
+            </View>
+          </View>
+
+          <View style = {styles.container19} >
+            <View style = {styles.container20} >
+              <Text style = {styles.container21} > Sign In </Text>
+            </View>
+          </View>
+
+          <Text style = {styles.container22} >
+            <Text style = {styles.container23} > Don’ t have account ? </Text>
+            <Text style={styles.container24}>Sign up here</Text > 
+          </Text>
+        </View>
+      </View>
     </View>
-    <View style = {
-      {
-        "alignItems": "flex-start",
-        "marginTop": 24.86
-      }
-    } >
-    <View style = {
-      {
-        "alignItems": "flex-start",
-        "paddingStart": 16,
-        "paddingTop": 22,
-        "width": 318,
-        "height": 336,
-        "borderRadius": 8,
-        "backgroundColor": "rgba(255, 255, 255, 255)"
-      }
-    } >
-    <View style = {
-      {
-        flexDirection: 'row',
-        alignItems: 'flex-start'
-      }
-    } > <Text style = {
-      {
-        "fontFamily": "Poppins",
-        "fontWeight": "bold",
-        "fontSize": 28,
-        "color": "rgba(43, 43, 43, 255)",
-        "marginStart": 75
-      }
-    } > Welcome </Text>
-    <Text style = {
-      {
-        "fontFamily": "Poppins",
-        "fontSize": 14,
-        "color": "rgba(43, 43, 43, 255)",
-        "marginStart": -133,
-        "marginTop": 35
-      }
-    } > Sign in to
-    continue </Text>
-      </View>
-      <View style = {
-        {
-          "alignItems": "flex-start",
-          "marginTop": 32
-        }
-      } >
-      <View style = {
-        {
-          "alignItems": "flex-start",
-          "paddingStart": 15,
-          "paddingTop": 8,
-          "width": 278,
-          "height": 35,
-          "borderRadius": 8,
-          "borderWidth": 1,
-          "borderColor": "rgba(235, 235, 242, 255)",
-          "backgroundColor": "rgba(252, 252, 255, 255)"
-        }
-      } >
-      <Text style = {
-        {
-          "fontFamily": "Poppins",
-          "fontSize": 14,
-          "color": "rgba(174, 175, 193, 255)"
-        }
-      } > Username </Text>
-      </View>
-      </View>
-      <View style = {
-        {
-          "alignItems": "flex-start",
-          "marginTop": 23
-        }
-      } >
-      <View style = {
-        {
-          "alignItems": "flex-start",
-          "paddingStart": 15,
-          "paddingTop": 8,
-          "width": 278,
-          "height": 35,
-          "borderRadius": 8,
-          "borderWidth": 1,
-          "borderColor": "rgba(235, 235, 242, 255)",
-          "backgroundColor": "rgba(252, 252, 255, 255)"
-        }
-      } >
-      <Text style = {
-        {
-          "fontFamily": "Poppins",
-          "fontSize": 14,
-          "color": "rgba(174, 175, 193, 255)"
-        }
-      } > Password </Text>
-      </View>
-      </View>
-      <View style = {
-        {
-          "alignItems": "flex-start",
-          "marginStart": 3,
-          "marginTop": 23
-        }
-      } >
-      <View style = {
-        {
-          "alignItems": "flex-start",
-          "paddingStart": 113,
-          "paddingTop": 6,
-          "width": 275,
-          "height": 35,
-          "borderRadius": 8,
-          "backgroundColor": "rgba(64, 163, 255, 255)"
-        }
-      } >
-      <Text style = {
-        {
-          "fontFamily": "Poppins",
-          "fontSize": 16,
-          "color": "rgba(255, 255, 255, 255)"
-        }
-      } > Sign In </Text>
-      </View>
-      </View>
-      <Text style = {
-        {
-          "marginStart": 42,
-          "marginTop": 14
-        }
-      } >
-      <Text style = {
-        {
-          "fontFamily": "Poppins",
-          "fontSize": 12,
-          "color": "rgba(33, 33, 33, 255)"
-        }
-      } > Don’ t have account ? </Text><Text style={{"fontFamily":"Poppins","fontSize":12,"color":"rgba(64, 163, 255, 255)"}}>Sign up here</Text > </Text>
-      </View>
-      </View>
-      </View>
-
   );
 };
---
+++ frontend/src/components/Login/Login.style.js
@@ -0,0 +1,152 @@
+
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+        "alignItems": "flex-start",
+        "paddingStart": 29,
+        "paddingTop": 49.1,
+        "flex": 1
+    },
+    container2: {
+        "alignItems": "flex-start",
+        "marginStart": 63
+    },
+    container3:{
+        "width": 8.83,
+        "height": 11.41,
+        "backgroundColor": "#000000"
+    },
+    container4:{
+        "width": 11.96,
+        "height": 8.29,
+        "backgroundColor": "#000000"
+    },
+    container5:{
+        "width": 11.18,
+        "height": 9.33,
+        "backgroundColor": "#000000"
+    },
+    container6:{
+        "alignItems": "flex-start",
+        "marginStart": 3.99,
+        "marginTop": 12.03,
+        "opacity": 0.1,
+        "width": 6.77,
+        "height": 6.77,
+        "borderRadius": 3.3853232860565186,
+        "transform": [{
+          "scaleX": 2.81
+        }],
+        "backgroundColor": "rgba(0, 0, 0, 255)"
+    },
+    container7:{
+        "width": 185.64,
+        "height": 139.04,
+        "backgroundColor": "#000000"
+    },
+    container8:{
+        "alignItems": "flex-start",
+        "marginTop": 24.86
+    },
+    container9:{
+        "alignItems": "flex-start",
+        "paddingStart": 16,
+        "paddingTop": 22,
+        "width": 318,
+        "height": 336,
+        "borderRadius": 8,
+        "backgroundColor": "rgba(255, 255, 255, 255)"
+    },
+    container10:{
+        flexDirection: 'row',
+        alignItems: 'flex-start'
+    },
+    container11:{
+        "fontFamily": "Poppins",
+        "fontWeight": "bold",
+        "fontSize": 28,
+        "color": "rgba(43, 43, 43, 255)",
+        "marginStart": 75
+    },
+    container12:{
+        "fontFamily": "Poppins",
+        "fontSize": 14,
+        "color": "rgba(43, 43, 43, 255)",
+        "marginStart": -133,
+        "marginTop": 35
+    },
+    container13:{
+        "alignItems": "flex-start",
+        "marginTop": 32
+    },
+    container14:{
+        "alignItems": "flex-start",
+        "paddingStart": 15,
+        "paddingTop": 8,
+        "width": 278,
+        "height": 35,
+        "borderRadius": 8,
+        "borderWidth": 1,
+        "borderColor": "rgba(235, 235, 242, 255)",
+        "backgroundColor": "rgba(252, 252, 255, 255)"
+    },
+    container15:{
+        "fontFamily": "Poppins",
+        "fontSize": 14,
+        "color": "rgba(174, 175, 193, 255)"
+    },
+    container16:{
+        "alignItems": "flex-start",
+        "marginTop": 23
+    },
+    container17:{
+        "alignItems": "flex-start",
+        "paddingStart": 15,
+        "paddingTop": 8,
+        "width": 278,
+        "height": 35,
+        "borderRadius": 8,
+        "borderWidth": 1,
+        "borderColor": "rgba(235, 235, 242, 255)",
+        "backgroundColor": "rgba(252, 252, 255, 255)"
+    },
+    container18:{
+        "fontFamily": "Poppins",
+        "fontSize": 14,
+        "color": "rgba(174, 175, 193, 255)"
+    },
+    container19:{
+        "alignItems": "flex-start",
+        "marginStart": 3,
+        "marginTop": 23
+    },
+    container20:{
+        "alignItems": "flex-start",
+        "paddingStart": 113,
+        "paddingTop": 6,
+        "width": 275,
+        "height": 35,
+        "borderRadius": 8,
+        "backgroundColor": "rgba(64, 163, 255, 255)"
+    },
+    container21:{
+        "fontFamily": "Poppins",
+        "fontSize": 16,
+        "color": "rgba(255, 255, 255, 255)"
+    },
+    container22:{
+        "marginStart": 42,
+        "marginTop": 14
+    },
+    container23:{
+        "fontFamily": "Poppins",
+        "fontSize": 12,
+        "color": "rgba(33, 33, 33, 255)"
+    },
+    container24:{
+        "fontFamily":"Poppins","fontSize":12,"color":"rgba(64, 163, 255, 255)"
+    }
+})
+
+export default styles;
